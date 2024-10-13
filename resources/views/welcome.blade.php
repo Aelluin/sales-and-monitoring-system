7ef5252043c76ed586e2d6fd31eeac254282170b
Laurence Ramirez <rhenz8823@gmail.com>
2024-10-13 17:31:04 +0800

    checkpoint 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -146,19 +146,65 @@
           <ul class="main-nav nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
             <li><a href="#">Hot Deals</a></li>
-            <li><a href="#">Trading Card Games</a></li>
-            <li><a href="#">Consoles</a></li>
-            <li><a href="#">Products</a></li>
-            <li><a href="#">Events</a></li>
-            <li><a href="#">Accessories</a></li>
+            <li><a href="#">Latest Promotions</a></li>
+            <li><a href="#">Top Selling</a></li>
+            <li><a href="#">About Us</a></li>
           </ul>
           <!-- /NAV -->
         </div>
         <!-- /responsive-nav -->
       </div>
       <!-- /container -->
+
     </nav>
+
     <!-- /NAVIGATION -->
+
+    <!-- HOT DEAL SECTION -->
+    <div id="hot-deal" class="section">
+        <!-- container -->
+        <div class="container">
+          <!-- row -->
+          <div class="row">
+            <div class="col-md-12">
+              <div class="hot-deal">
+                <ul class="hot-deal-countdown">
+                  <li>
+                    <div>
+                      <h3>02</h3>
+                      <span>Days</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <h3>10</h3>
+                      <span>Hours</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <h3>34</h3>
+                      <span>Mins</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <h3>60</h3>
+                      <span>Secs</span>
+                    </div>
+                  </li>
+                </ul>
+                <h2 class="text-uppercase">hot deal this week</h2>
+                <p>New Collection Up to 50% OFF</p>
+                <a class="primary-btn cta-btn" href="#">Shop now</a>
+              </div>
+            </div>
+          </div>
+          <!-- /row -->
+        </div>
+        <!-- /container -->
+      </div>
+      <!-- /HOT DEAL SECTION -->
 
     <!-- SECTION -->
     <div class="section">
@@ -374,53 +420,7 @@
     </div>
     <!-- /SECTION -->
 
-    <!-- HOT DEAL SECTION -->
-    <div id="hot-deal" class="section">
-      <!-- container -->
-      <div class="container">
-        <!-- row -->
-        <div class="row">
-          <div class="col-md-12">
-            <div class="hot-deal">
-              <ul class="hot-deal-countdown">
-                <li>
-                  <div>
-                    <h3>02</h3>
-                    <span>Days</span>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <h3>10</h3>
-                    <span>Hours</span>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <h3>34</h3>
-                    <span>Mins</span>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <h3>60</h3>
-                    <span>Secs</span>
-                  </div>
-                </li>
-              </ul>
-              <h2 class="text-uppercase">hot deal this week</h2>
-              <p>New Collection Up to 50% OFF</p>
-              <a class="primary-btn cta-btn" href="#">Shop now</a>
-            </div>
-          </div>
-        </div>
-        <!-- /row -->
-      </div>
-      <!-- /container -->
-    </div>
-    <!-- /HOT DEAL SECTION -->
 
-    <!-- SECTION -->
     <div class="section">
       <!-- container -->
       <div class="container">
@@ -431,14 +431,7 @@
             <div class="section-title">
               <h3 class="title">Top selling</h3>
               <div class="section-nav">
-                <ul class="section-tab-nav tab-nav">
-                  <li class="active">
-                    <a data-toggle="tab" href="#tab2">Laptops</a>
-                  </li>
-                  <li><a data-toggle="tab" href="#tab2">Smartphones</a></li>
-                  <li><a data-toggle="tab" href="#tab2">Cameras</a></li>
-                  <li><a data-toggle="tab" href="#tab2">Accessories</a></li>
-                </ul>
+
               </div>
             </div>
           </div>
@@ -454,19 +447,19 @@
                     <!-- product -->
                     <div class="product">
                       <div class="product-img">
-                        <img src="./img/product06.png" alt="" />
+                        <img src="./img/t1.jpg" alt="" />
                         <div class="product-label">
-                          <span class="sale">-30%</span>
-                          <span class="new">NEW</span>
+                          <!---<span class="sale">-30%</span>
+                          <span class="new">NEW</span> -->
                         </div>
                       </div>
                       <div class="product-body">
-                        <p class="product-category">Category</p>
+                        <p class="product-category">Figures</p>
                         <h3 class="product-name">
-                          <a href="#">product name goes here</a>
+                          <a href="#">One piece gear 5 luffy</a>
                         </h3>
                         <h4 class="product-price">
-                          $980.00 <del class="product-old-price">$990.00</del>
+                            ₱19,999.00 <del class="product-old-price">₱24,999</del>
                         </h4>
                         <div class="product-rating">
                           <i class="fa fa-star"></i>
@@ -490,11 +483,7 @@
                           </button>
                         </div>
                       </div>
-                      <div class="add-to-cart">
-                        <button class="add-to-cart-btn">
-                          <i class="fa fa-shopping-cart"></i> add to cart
-                        </button>
-                      </div>
+
                     </div>
                     <!-- /product -->
 
@@ -1099,113 +1088,26 @@
         <div class="container">
           <!-- row -->
           <div class="row">
-            <div class="col-md-3 col-xs-6">
-              <div class="footer">
-                <h3 class="footer-title">About Us</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut.
-                </p>
-                <ul class="footer-links">
-                  <li>
-                    <a href="#"
-                      ><i class="fa fa-map-marker"></i>1734 Stonecoal Road</a
-                    >
-                  </li>
-                  <li>
-                    <a href="#"><i class="fa fa-phone"></i>+021-95-51-84</a>
-                  </li>
-                  <li>
-                    <a href="#"
-                      ><i class="fa fa-envelope-o"></i>email@email.com</a
-                    >
-                  </li>
-                </ul>
-              </div>
-            </div>
+            <div class="col-md-3 col-xs-6" style="text-align: center;">
 
-            <div class="col-md-3 col-xs-6">
-              <div class="footer">
-                <h3 class="footer-title">Categories</h3>
-                <ul class="footer-links">
-                  <li><a href="#">Hot deals</a></li>
-                  <li><a href="#">Laptops</a></li>
-                  <li><a href="#">Smartphones</a></li>
-                  <li><a href="#">Cameras</a></li>
-                  <li><a href="#">Accessories</a></li>
-                </ul>
-              </div>
-            </div>
 
-            <div class="clearfix visible-xs"></div>
-
-            <div class="col-md-3 col-xs-6">
-              <div class="footer">
-                <h3 class="footer-title">Information</h3>
-                <ul class="footer-links">
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Contact Us</a></li>
-                  <li><a href="#">Privacy Policy</a></li>
-                  <li><a href="#">Orders and Returns</a></li>
-                  <li><a href="#">Terms & Conditions</a></li>
-                </ul>
-              </div>
-            </div>
-
-            <div class="col-md-3 col-xs-6">
-              <div class="footer">
-                <h3 class="footer-title">Service</h3>
-                <ul class="footer-links">
-                  <li><a href="#">My Account</a></li>
-                  <li><a href="#">View Cart</a></li>
-                  <li><a href="#">Wishlist</a></li>
-                  <li><a href="#">Track My Order</a></li>
-                  <li><a href="#">Help</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <!-- /row -->
-        </div>
-        <!-- /container -->
-      </div>
-      <!-- /top footer -->
 
       <!-- bottom footer -->
-      <div id="bottom-footer" class="section">
+
         <div class="container">
           <!-- row -->
           <div class="row">
             <div class="col-md-12 text-center">
-              <ul class="footer-payments">
-                <li>
-                  <a href="#"><i class="fa fa-cc-visa"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="fa fa-credit-card"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="fa fa-cc-paypal"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="fa fa-cc-mastercard"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="fa fa-cc-discover"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="fa fa-cc-amex"></i></a>
-                </li>
-              </ul>
+
               <span class="copyright">
                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 Copyright &copy;
                 <script>
                   document.write(new Date().getFullYear());
                 </script>
-                All rights reserved | This template is made with
-                <i class="fa fa-heart-o" aria-hidden="true"></i> by
-                <a href="https://colorlib.com" target="_blank">Colorlib</a>
+               All Rights Reserved | Made with
+                <i class="fa fa-heart-o" aria-hidden="true"></i> by Yoshino Eight.
+                <!--<a href="https://colorlib.com" target="_blank">Colorlib</a>
                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
               </span>
             </div>
